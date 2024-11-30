f9554c3233c23263d6c87181be22a134df21b0f4
Amin_Foroutan <aminforoutan7@gmail.com>
2024-11-30 22:50:06 +0330

    [feat]: Add interview questions

---
+++ src/question1.ts
@@ -0,0 +1,10 @@
+/**
+ * Make This work
+ * 
+ * duplicate([1,2,3,4,5]) // [1,2,3,4,5,1,2,3,4,5]
+ * 
+ */
+
+export function duplicate(number: number[]): number[]{
+  throw new Error('No Implemented')
+}
---
+++ src/question2.ts
@@ -0,0 +1,6 @@
+/**
+ * 
+ * a function that iterates over 0-100 and logs "fizz" on multiples of 3 and buzz on multiples of 5 and fizzbuzz on multiples of 3 and 5
+ */
+
+function fizzBuzz(){}
